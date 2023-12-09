094dfa12628885d185cc50eab14319e502e9d409
Bruno Guerreiro <guerreiro.bms@gmail.com>
2023-12-09 09:26:51 +0000

    timeline

--- streamlit_guerreiro.py
+++ streamlit_guerreiro.py
@@ -72,38 +72,10 @@ with tab1:
         data = f.read()
     timeline(data, height=600)
 
-with tab2:
-    t = 'A faster way to build and share data apps'
-    html_style = '''
-    <style>
-    div:has( >.element-container div.floating) {
-        display: flex;
-        flex-direction: column;
-        position: fixed;
-    }
-
-    div.floating {
-        height:0%;
-    }
-    </style>
-    '''
-    st.markdown(html_style, unsafe_allow_html=True)
-
-    col1, col2 = st.columns([9, 2])
-
-    with col1:
-        for i in range(0, 30):
-            st.header("Today's news")
-            st.write(t)
-
-    with col2:
-        st.markdown(
-            '''
-                <div class="floating">
-                    <a href='https://streamlit.io/'>Hello Streamlit</a>
-                    <a href='https://streamlit.io/'>Hello Streamlit</a>
-                    <a href='https://streamlit.io/'>Hello Streamlit</a>
-                </div>
-            ''',
-            unsafe_allow_html=True
-        )
+with tab2: st.write("Under construction.")
+with tab3: st.write("Under construction.")
+with tab4: st.write("Under construction.")
+with tab5: st.write("Under construction.")
+with tab6: st.write("Under construction.")
+with tab7: st.write("Under construction.")
+with tab8: st.write("Under construction.")
